e3593c6d34f0b60ce0d574192d01fe7b84e3dc61
eijin kim <jianchidaodi1029@gmail.com>
2023-06-27 09:38:29 +0900

    main

--- src/App.js
+++ src/App.js
@@ -1,23 +1,61 @@
+import React, { useState } from 'react';
 import logo from './logo.svg';
 import './App.css';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+      <header className="nav">
+        <span className="logo">한줄일기</span>
+        <div className="buttons">
+          <button className="loginBtn">로그인</button>
+          <button className="joinBtn">회원가입</button>
+        </div>
       </header>
+      <div className="modalLogin">
+        <form className="modalContents">
+          아이디 <input className="inputId" type="text" /> <br></br>
+          비밀번호 <input className="inputPw" type="password" />
+          <br></br>
+          <button className="loginBtn">로그인</button>
+          <button className="cancelBtn">취소</button>
+        </form>
+      </div>
+      <div className="list">
+        <div className="newsfeed">
+          <h3> 하루일과 기록 </h3>
+          <p>
+            Lorem ipsum dolor sit, amet consectetur adipisicing elit. Ipsam quae aliquam asperiores
+            labore, ratione recusandae enim odio veritatis explicabo vero quos magni alias animi
+            adipisci dolore illo similique, aut tempora!Lorem ipsum dolor sit, amet consectetur
+            adipisicing elit. Ipsam quae aliquam asperiores labore, ratione recusandae enim odio
+            veritatis explicabo vero quos magni alias animi adipisci dolore illo similique, aut
+            tempora!{' '}
+          </p>
+        </div>
+        <div className="newsfeed">
+          <h3> 하루일과 기록 </h3>
+          <p>
+            Lorem ipsum dolor sit, amet consectetur adipisicing elit. Ipsam quae aliquam asperiores
+            labore, ratione recusandae enim odio veritatis explicabo vero quos magni alias animi
+            adipisci dolore illo similique, aut tempora!Lorem ipsum dolor sit, amet consectetur
+            adipisicing elit. Ipsam quae aliquam asperiores labore, ratione recusandae enim odio
+            veritatis explicabo vero quos magni alias animi adipisci dolore illo similique, aut
+            tempora!{' '}
+          </p>
+        </div>
+        <div className="newsfeed">
+          <h3> 하루일과 기록 </h3>
+          <p>
+            Lorem ipsum dolor sit, amet consectetur adipisicing elit. Ipsam quae aliquam asperiores
+            labore, ratione recusandae enim odio veritatis explicabo vero quos magni alias animi
+            adipisci dolore illo similique, aut tempora!Lorem ipsum dolor sit, amet consectetur
+            adipisicing elit. Ipsam quae aliquam asperiores labore, ratione recusandae enim odio
+            veritatis explicabo vero quos magni alias animi adipisci dolore illo similique, aut
+            tempora!{' '}
+          </p>
+        </div>
+      </div>
     </div>
   );
 }
